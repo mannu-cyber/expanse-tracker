75cd2a968fa7002329f90aa31452dbaebe3925cd
cyber-mannu <sahanimannu844@gmail.com>
2024-07-11 07:55:15 +0530

    removed nodemon.json file

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-console.log("Hello via Bun!");
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//  connect to DB
+
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI);
+
+  if (conn) {
+    console.log("MongoDB connected successfully");
+  }
+};
+
+connectDB();
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on Port ${PORT}`);
+});
